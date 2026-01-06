6643b91a0df1d1eda03ca78da081deef5206071b
shiba <shibathedeveloper+github@gmail.com>
2026-01-06 22:07:05 +0100

    add boxes

--- modules/data/boxes.lua
+++ modules/data/boxes.lua
@@ -1,5 +1,5 @@
 return {
-    [1] = {
+    {
         spritePath = "assets/sprites/gerald.png",
 
         description = "C'mon, it's Gerald! Do I really need to describe him? He's perfect!",
@@ -10,37 +10,114 @@ return {
         scale = .7,
         tier = 1,
     },
-    [2] = {
+    {
         spritePath = "assets/sprites/jimbo.png",
 
         description = "Jimbo is constantly shocked at everything, constantly horrified of what'll happen next.",
         quote = "WHAT??",
         name = "Jimbo",
 
-        weight = 60,
+        weight = 50,
         scale = .8,
         tier = 2,
     },
-    [3] = {
+    {
         spritePath = "assets/sprites/glumbo.png",
 
         description = "Glumbo's constantly planning on taking over the world, but that'll never happen because he's telling his plans to everyone. He's the reason for his brother Jimbo's constant shock.",
         quote = "And that's how I'll take over the world!",
         name = "Glumbo",
 
-        weight = 80,
+        weight = 55,
         scale = .9,
         tier = 3,
     },
-    [4] = {
+    {
         spritePath = "assets/sprites/jeremy.png",
 
         description = "Jeremy loves to remind the teacher about the homework. Nobody likes Jeremy.",
         quote = "Ermm.. actually.. ☝️🤓",
         name = "Jeremy",
 
-        weight = 100,
+        weight = 120,
         scale = 1,
         tier = 4,
+    },
+    {
+        spritePath = "assets/sprites/muncher.png",
+
+        description = "An overweight box that most definitely ate that cookie.",
+        quote = "I didn't eat that cookie!",
+        name = "Muncher",
+
+        weight = 250,
+        scale = 1.1,
+        tier = 5,
+    },
+    {
+        spritePath = "assets/sprites/dylan.png",
+
+        description = [[Dylan loves clothes shopping at "Cold Subject". He isn't very talkative.]],
+        quote = "...",
+        name = "Dylan",
+
+        weight = 255,
+        scale = 1.2,
+        tier = 6,
+    },
+    {
+        spritePath = "assets/sprites/carlos.png",
+
+        description = "A really enthusiastic box, loves bothering people. Annoying.",
+        quote = "HEY!!!",
+        name = "Carlos",
+
+        weight = 270,
+        scale = 1.3,
+        tier = 7,
+    },
+    {
+        spritePath = "assets/sprites/goobster.png",
+
+        description = "This goofy goober of a box loves sticking out his tongue! A very silly creature indeed.",
+        quote = "Blehhhh!",
+        name = "Goobster Goobington III",
+
+        weight = 300,
+        scale = 1.4,
+        tier = 8,
+    },
+    {
+        spritePath = "assets/sprites/angel.png",
+
+        description = "Goodness gracious!! Is this thing okay!?!? It's eyes are bloodshot!!",
+        quote = "YASS QWEEN 💅💅 WE'RE GETTING SOME GEMINI ENERGY! SLAYY! 💅💅💅",
+        name = "Angel",
+
+        weight = 400,
+        scale = 1.5,
+        tier = 9,
+    },
+    {
+        spritePath = "assets/sprites/gochged.png",
+
+        description = "A granite-man transformed into a box. He's the creator of the faces you see on the boxes.",
+        quote = "ROCK NOISES!!!",
+        name = "Gochged",
+
+        weight = 600,
+        scale = 1.6,
+        tier = 10,
+    },
+    {
+        spritePath = "assets/sprites/dizzy.png",
+
+        description = "A dizzy cube which bears a striking resemblance to the creator of the game.",
+        quote = "Whuh? wha-?",
+        name = "Dizzy",
+
+        weight = 450,
+        scale = 1.7,
+        tier = 10,
     },
 }
