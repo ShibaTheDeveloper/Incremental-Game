6697aa5c177ca23e7fd43edf2440aa0b4b6bd8d1
shiba <shibathedeveloper+github@gmail.com>
2026-01-07 20:38:12 +0100

    spawner.lua -> factory.lua

--- code/game/box/factory.lua
+++ code/game/box/factory.lua
@@ -1,4 +1,4 @@
--- ~/code/game/box/spawner.lua
+-- ~/code/game/box/factory.lua
 
 local BoxesObjectModule = require("code.game.box.object")
 local extra = require("code.engine.extra")
@@ -7,11 +7,11 @@ local CONSTANTS = require("code.game.box.constants")
 
 local Module = {}
 
-function Module:spawn(tier)
+function Module:spawn()
     local x = math.random(-CONSTANTS.AREA_WIDTH, CONSTANTS.AREA_WIDTH)
     local y = math.random(-CONSTANTS.AREA_HEIGHT, CONSTANTS.AREA_HEIGHT)
 
-    local data = BoxesObjectModule:getBoxDataByTier(tier, x, y)
+    local data = BoxesObjectModule:getBoxDataByTier(1, x, y)
     local box = BoxesObjectModule:createBox(data)
 
     local velocityX = math.random(CONSTANTS.MIN_SPAWN_VELOCITY, CONSTANTS.MAX_SPAWN_VELOCITY)
